6b316cd9c9547431b54f93f1906a8f2959d5cc67
Carlos Fdez. Llamas <hello@sirikon.me>
2019-12-23 16:13:42 +0100

    Remove unwanted output

--- app/out/src/web/controllers/homeController.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-async function homeController(ctx) {
-    await ctx.render('home');
-}
-exports.default = homeController;
