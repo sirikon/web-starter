5a9e78e92802c0995059285a9b9be47220f68d7a
Carlos Fdez. Llamas <hello@sirikon.me>
2019-12-25 04:15:43 +0100

    Forgot to uncomment some loggers

--- app/src/migrator.ts
+++ app/src/migrator.ts
@@ -2,8 +2,10 @@ import { Gratin, Postgres } from 'gratin';
 import { join } from 'path';
 import waitOn from 'wait-on';
 
+import { JobContext } from 'application/models';
 import config from 'config';
-import logger from 'logging/logger';
+import Logger from 'logging/logger';
+import { container } from 'tsyringe';
 
 export async function migrate(): Promise<void> {
 	await waitDatabaseToBeAvailable();
@@ -21,12 +23,19 @@ export async function migrate(): Promise<void> {
 }
 
 async function waitDatabaseToBeAvailable(): Promise<void> {
+	const logger = getLogger();
 	const host = config.postgres.host;
 	const port = config.postgres.port;
-	// logger.info('Waiting for database to be available', { host, port });
+	logger.info('Waiting for database to be available', { host, port });
 	await waitOn({
 		resources: [`tcp:${host}:${port}`],
 		timeout: 30000,
 	});
-	// logger.info('Database available', { host, port });
+	logger.info('Database available', { host, port });
+}
+
+function getLogger(): Logger {
+	const childContainer = container.createChildContainer();
+	childContainer.register(JobContext, { useValue: new JobContext('migrator') });
+	return childContainer.resolve(Logger);
 }
